1b32924f3ed1d9bbb035d2ecd6b396f35918fe4a
shoroburof <shorobutaevgenij@gmail.com>
2025-07-06 12:59:12 +0300

    seo upd

--- src/app/product/[id]/page.js
+++ src/app/product/[id]/page.js
@@ -15,13 +15,61 @@ async function getData(id) {
 
     return info.data;
   }
+
+  return null
 }
 
 
 export async function generateMetadata({params,searchParams}) {
+  const info = await getData(params.id);
+
+  if(!info) {
+    return {
+      title: "Товар не знайдено",
+      description: 'Вибачте це оголошення видалено або архівовано'
+    };
+  }
+  
+  const {
+    name ,
+    description,
+    image_array
+  } = info;
+
+  const noImage = `${process.env.NEXT_PUBLIC_URL}/assets/background.webp`;
+  const firstImage = image_array?.[0]
+  ? `${process.env.NEXT_PUBLIC_URL}/uploads${image_array[0]}`
+  : noImage;
+
+
+
   return {
-    title: "Red Magic 6R",
-    description: "Представляем вам шикарную игровую новинку от китайской компании ЗТЕ. Отличный, мощный гаджет прекрасно подойдёт для мобильного гейминга и работы с мощными приложениями. Стоит ли купить Nubia Red Magic 6R, соответствует ли цена качеству и где можно приобрести девайс максимально недорого? Обо всем этом вы узнаете на этой странице",
+    title: name,
+    description: description?.slice(0,160) || "Знаходь то що треба на Kando",
+    openGraph: {
+      title: `${name}`,
+      description: description?.slice(0,160),
+      url: `${process.env.NEXT_PUBLIC_URL}/product/${params.id}`,
+      siteName: "Kando",
+      images: [
+        {
+          url: firstImage,
+          width: 800,
+          height: 600,
+          alt: name
+        }
+      ],
+      locale: "uk-UA",
+      type: "product"
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `${name}`,
+      description: description?.slice(0,160),
+      images: [
+        firstImage
+      ]
+    }
   }
 }
 
