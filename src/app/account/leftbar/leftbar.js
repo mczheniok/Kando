1c11514b3d385dadc10c145dfad97ab8b17207e7
shoroburof <shorobutaevgenij@gmail.com>
2025-07-08 19:41:49 +0300

    hot fix

--- src/app/account/leftbar/leftbar.js
+++ src/app/account/leftbar/leftbar.js
@@ -24,8 +24,13 @@ const listTargets = [
     {title: "Історія",icon: <HistoryIcon width={25} height={25} />}
 ]
 
-const UserAvatar = ({width,height,padding=null,src="/assets/noimage.webp"}) => <Image width={width} height={height} src={src} style={{padding: padding?padding:"7px",background: "#e5e4e2",border:"none",boxSizing: "content-box"}} className="circle" alt="User Avatar"></Image>
+const UserAvatar = ({width,height,padding=null,src="/assets/noimage.webp"}) => {
+    const [imgUrl,setImgUrl] = useState(`${process.env.NEXT_PUBLIC_URL}/${src}`);
 
+    return (
+        <Image width={width} height={height} src={imgUrl} onError={() => setImgUrl("/assets/noimage.webp")} style={{padding: padding?padding:"7px",background: "#e5e4e2",border:"none",boxSizing: "content-box"}} className="circle" alt="User Avatar"></Image>
+    )
+}
 
 export const MoreButton = ({userData}) => {
     const [more,SetMore] = useState(false);
@@ -33,7 +38,7 @@ export const MoreButton = ({userData}) => {
     return (
         <div className={`${styles.ButtonAcaunt} flex flex-row align-center justify-between`}>
             <Suspense fallback={<Loading time={0} />}>
-            <UserAvatar padding={"0px"} src={`${process.env.NEXT_PUBLIC_URL}${userData?.image}`}  width={30} height={30}></UserAvatar>
+            <UserAvatar padding={"0px"} src={userData?.image}  width={30} height={30}></UserAvatar>
             <div className="flex flex-col align-start" style={{gap: '.1rem',width: "fit-content",maxWidth: "56% "}}>
                 <h4 className="small-text">{userData?.name}</h4>
                 <h5>{userData?.email?.slice(0,10) || "email"}</h5>
@@ -71,7 +76,7 @@ export function LeftBar({visible,ref,userData,set,close}) {
         <aside className={`${styles.aside} ${visible ? styles.visible : ""} flex-col align-center`}>
             <ul data-id={"asideList"} ref={ref} className={`${styles.asideList} flex-col justify-around align-center`} style={{marginTop: "1rem"}}>
                 <Suspense fallback={<Loading/>}>
-                    <UserAvatar src={`${process.env.NEXT_PUBLIC_URL}${userData?.image}`} width={100} height={100}></UserAvatar>
+                    <UserAvatar src={userData?.image} width={100} height={100}></UserAvatar>
                 </Suspense>
                 {listTargets.map((el,ind) => {
                     return (
