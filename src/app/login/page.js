045e5733396a7444fb9c5bdbddb883658de7b551
shoroburof <shorobutaevgenij@gmail.com>
2025-07-12 23:12:59 +0300

    update

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,8 +1,11 @@
 "use client"
+import { Suspense } from "react";
 import AuthWrapper from "./authwrapper";
 
 export default function Page() {
     return (
-        <AuthWrapper></AuthWrapper>    
+        <Suspense fallback={<div style={{minHeight: "100%"}}>Loading</div>}>
+            <AuthWrapper></AuthWrapper>    
+        </Suspense>
     )  
 }
